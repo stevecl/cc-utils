ecec3a1554bebe8d241dbe7746b04734b00de77f
cc <1040457762@qq.com>
2022-05-05 00:35:18 +0800

    modify

--- maytek-asahi-mini/pages/lottery/index.js
+++ maytek-asahi-mini/pages/lottery/index.js
@@ -33,8 +33,49 @@ Page({
       .exec(async (res) => {     //async 和 await 成对出现
         const canvas = res[0].node;
         const context = canvas.getContext('2d');
-        console.log('ctx', context)
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = res[0].width * dpr
+        canvas.height = res[0].height * dpr
+        // context.scale(dpr, dpr)
+        const circle = {
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+            radius: 180
+        }
+        console.log('ctx', context, circle)
         // canvas.setFontSize(40)
+        // 矩形pink和矩形orange之间相隔20px
+        // context.translate(200 + Math.cos(60) * 50, 200 + Math.sin(60) * 100);
+        // context.rotate(30);
+        context.fillStyle = "white"
+				context.font="26px sans-serif"
+        context.fillText("撒大声地", circle.x, circle.y);
+        // return
+        // 1.6 0 3.2
+        let string = '撒大程序没能力肯定积分克里斯多夫',
+        startAngle = Math.PI*2+Math.PI/2, endAngle = Math.PI/8+Math.PI/2,  angle = 4.8, index = 0, character = '',
+        angleDecrement = (startAngle - endAngle) / (40 - 1)
+        console.log('angleDecrement', angleDecrement)
+        let radius = circle.radius
+
+        while(index < string.length){
+          //获取传入的字符串的每个字符
+          character = string.charAt(index);
+          context.save();
+          context.beginPath();
+          //位移到每个字符的指定位置
+          context.translate(circle.x+Math.cos(angle)*radius,circle.y - Math.sin(angle)*radius);
+          console.log('angle', angle)
+
+          //旋转坐标系到每个字符应该达到到角度
+          context.rotate(Math.PI/2 - angle);
+          context.fillText(character,0,0);
+          context.strokeText(character,0,0);
+          //角度递减
+          angle -= angleDecrement;
+          index++;
+          context.restore();
+        }
       })
   },
 
